6ac81ee95548292272ee273f826a1cbdde4534a3
Shmuel Kallner <kallner@il.ibm.com>
2026-05-14 18:21:38 +0300

    Initial config loader and plugin instantiator
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

---
+++ pkg/config/loader/configloader.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2026 The llm-d Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package loader
+
+import (
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/sets"
+
+	configapi "github.com/llm-d/llm-d-inference-payload-processor/apix/config/v1alpha1"
+	config "github.com/llm-d/llm-d-inference-payload-processor/pkg/config"
+	"github.com/llm-d/llm-d-inference-payload-processor/pkg/framework"
+)
+
+var (
+	scheme = runtime.NewScheme()
+)
+
+func init() {
+	utilruntime.Must(configapi.Install(scheme))
+}
+
+func LoadConfiguration(configBytes []byte, handle framework.Handle, logger logr.Logger) (*config.Config, error) {
+	rawConfig, err := loadRawConfiguration(configBytes, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = instantiatePlugins(rawConfig.Plugins, handle); err != nil {
+		return nil, err
+	}
+
+	return nil, err
+}
+
+func loadRawConfiguration(configBytes []byte, logger logr.Logger) (*configapi.PayloadProcessorConfig, error) {
+	var rawConfig *configapi.PayloadProcessorConfig
+	var err error
+	if len(configBytes) != 0 {
+		rawConfig = &configapi.PayloadProcessorConfig{}
+		codecs := serializer.NewCodecFactory(scheme, serializer.EnableStrict)
+		if err := runtime.DecodeInto(codecs.UniversalDecoder(), configBytes, rawConfig); err != nil {
+			return nil, fmt.Errorf("failed to decode configuration JSON/YAML: %w", err)
+		}
+		logger.Info("Loaded raw configuration", "config", rawConfig.String())
+	} else {
+		logger.Info("A configuration wasn't specified. A default one is being used.")
+		rawConfig = loadDefaultConfig()
+		logger.Info("Default raw configuration used", "config", rawConfig.String())
+	}
+
+	applyRawConfigDefaults(rawConfig)
+
+	return rawConfig, err
+}
+
+func instantiatePlugins(configuredPlugins []configapi.PluginSpec, handle framework.Handle) error {
+	pluginNames := sets.New[string]()
+	for _, spec := range configuredPlugins {
+		if spec.Type == "" {
+			return fmt.Errorf("plugin '%s' is missing a type", spec.Name)
+		}
+		if pluginNames.Has(spec.Name) {
+			return fmt.Errorf("duplicate plugin name '%s'", spec.Name)
+		}
+		pluginNames.Insert(spec.Name)
+
+		factory, ok := framework.Registry[spec.Type]
+		if !ok {
+			return fmt.Errorf("plugin type '%s' is not registered", spec.Type)
+		}
+		plugin, err := factory(spec.Name, spec.Parameters, handle)
+		if err != nil {
+			return fmt.Errorf("failed to create plugin '%s' (type: %s): %w", spec.Name, spec.Type, err)
+		}
+
+		handle.AddPlugin(spec.Name, plugin)
+	}
+
+	return nil
+}
