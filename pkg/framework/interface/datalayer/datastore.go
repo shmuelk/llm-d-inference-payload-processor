b45b8019de4ecca82526fe37cc98d3a7a786e7ac
Shmuel Kallner <kallner@il.ibm.com>
2026-05-21 14:46:00 +0300

    Moved Datastore interface to appropriate place
    Signed-off-by: Shmuel Kallner <kallner@il.ibm.com>

--- pkg/framework/interface/datalayer/datastore.go
+++ pkg/framework/interface/datalayer/datastore.go
@@ -14,13 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package datastore
-
-import "github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/interface/datalayer"
+package datalayer
 
 // Datastore is the interface for reading and updating the model store.
 type Datastore interface {
-	GetOrCreateModel(name string) datalayer.Model
+	GetOrCreateModel(name string) Model
 	DeleteModel(name string)
 	Models() []string
 }
